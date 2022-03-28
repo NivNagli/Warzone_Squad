45aab7d0f1738bfe11ac7c6a78b3b4e674204b8c
Niv Nagli <itsniv@gmail.com>
2022-03-28 16:18:48 +0300

    add getUserData method, for signed users

--- frontend/src/Middlewares/backend-requests.js
+++ frontend/src/Middlewares/backend-requests.js
@@ -2,7 +2,7 @@ import API_PREFIX from '../sharedUrls';
 
 const makeBattleUrl = (username) => {
     let splittedUsername = username.split('#');
-    if(splittedUsername.length === 2) {
+    if (splittedUsername.length === 2) {
         return `${API_PREFIX}/profile/username/${splittedUsername[0] + "%2523" + splittedUsername[1]}/platform/battle`;
     };
     // The case someone enter invalid username.
@@ -11,7 +11,7 @@ const makeBattleUrl = (username) => {
 
 const makeBattleUsername = (username) => {
     let splittedUsername = username.split('#');
-    if(splittedUsername.length === 2) {
+    if (splittedUsername.length === 2) {
         return `${splittedUsername[0] + "%2523" + splittedUsername[1]}`;
     };
     return "InvalidUserName";
@@ -22,7 +22,7 @@ export const playerSearchAttempt = async (username, platform, sendRequest) => {
         /* Using our http custom hook in order to send the request and update the 'isLoading', 'error'
          * States that the hook produce for us */
         let url = `${API_PREFIX}/profile/username/${username}/platform/${platform}`;
-        if(platform === 'battle') {
+        if (platform === 'battle') {
             url = makeBattleUrl(username);
         };
         const responseData = await sendRequest(
@@ -44,7 +44,7 @@ export const playerSearchAttempt = async (username, platform, sendRequest) => {
 
 const fixBattleUsernames = (usernames, platforms) => {
     platforms.forEach((platform, index) => {
-        if(platform === 'battle') {
+        if (platform === 'battle') {
             usernames[index] = makeBattleUsername(usernames[index]);
         }
     });
@@ -56,8 +56,8 @@ export const playersCompareAttempt = async (usernames, platforms, sendRequest) =
         fixBattleUsernames(usernamesCopy, platforms);
         const url = `${API_PREFIX}/squad/compare`;
         const reqBody = {
-            usernames : usernamesCopy,
-            platforms : platforms
+            usernames: usernamesCopy,
+            platforms: platforms
         };
         const responseData = await sendRequest(
             url, // URL
@@ -94,3 +94,25 @@ export const matchSearchAttempt = async (matchID, sendRequest) => {
         return null;
     }
 };
+
+export const getUserData = async (token, sendRequest) => {
+    try {
+        const url = `${API_PREFIX}/user/get-user-data`;
+        const reqBody = {};
+        const responseData = await sendRequest(
+            url, // URL
+            'GET', // METHOD
+            reqBody,
+            { // HEADERS
+                Authorization: 'Bearer ' + token
+            },
+            "Failed to find the player profile, Please try again, [SPP]." // DEFAULT ERROR MSG SPP = server problem possibility.
+        );
+        return responseData; // The case the user enter valid credentials.
+    }
+    catch (e) {
+        console.log(`err__login = ${e}`); // The case the user entered invalid credentials / server problem.
+        return null;
+    }
+};
+
